813f446f48cd22001e6d8aa26d7744e814aebe5e
mayinot <31657679+mayinot@users.noreply.github.com>
2022-09-18 00:55:38 +0800

    typos

--- venture-app/pages/index.tsx
+++ venture-app/pages/index.tsx
@@ -29,7 +29,7 @@ const features = [
     icon: HandshakeIcon,
   },
   {
-    name: 'Mobile notifications',
+    name: 'Open Communications',
     description:
       'Support both investors and inspiring start up achieve their dreams. Venture team give you back some time so that you can focus on the important things.',
     icon: ChatIcon,
@@ -77,13 +77,12 @@ const Home:NextPage  = () => {
       <div className="bg-white py-12">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-lg font-semibold text-indigo-600">Transactions</h2>
+          <h2 className="text-lg font-semibold text-indigo-600">Learn More</h2>
           <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
+            Ideas into realities
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-            accusamus quisquam.
+          Together, we’re on a mission to raise global economic prosperity through the frictionless exchange of financial value.
           </p>
         </div>
 
