e24e60e979489fd16f3ca909eb7ed314e752feab
cweiwenr <reubenchongww2512@gmail.com>
2022-10-16 14:30:42 +0800

    deployment fix 5

--- venture-app/pages/invest/[id].tsx
+++ venture-app/pages/invest/[id].tsx
@@ -5,11 +5,8 @@ import { ProjectProps } from "../../components/Layout/ProjectCard";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "../../components/Layout/CheckoutForm";
 import useUser from "../../lib/useUser";
-import Image from 'next/image'
+import Image from "next/image";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-);
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const project_id = ctx.query.id;
   const proj_id_num: number | null = +project_id!;
@@ -31,34 +28,44 @@ const Project: React.FC<ProjectProps> = (props: any) => {
         <div>
           {
             <>
-            <h2 className="pt-10 pb-4 px-4 text-3xl font-bold mr-4 sm:text-4xl">Title: {props.project.title}</h2>
-            <div className="grid grid-cols-3 gap-16 py-0 px-10">
-              <div className="col-span-2">
-              <h3 className=" font-bold text-lg mr-4 sm:text-xl">Amount Raised: {props.project.raise_amt}</h3>
-                <div className="text-lg font-bold">Description:</div> <p>{props.project.description}</p>
-                <div className="text-lg font-bold ">Business Model:</div> <p>{props.project.busi_model}</p>
-                <div className="text-lg font-bold">Highlights:</div>  <p>{props.project.highlights}</p>
+              <h2 className="pt-10 pb-4 px-4 text-3xl font-bold mr-4 sm:text-4xl">
+                Title: {props.project.title}
+              </h2>
+              <div className="grid grid-cols-3 gap-16 py-0 px-10">
+                <div className="col-span-2">
+                  <h3 className=" font-bold text-lg mr-4 sm:text-xl">
+                    Amount Raised: {props.project.raise_amt}
+                  </h3>
+                  <div className="text-lg font-bold">Description:</div>{" "}
+                  <p>{props.project.description}</p>
+                  <div className="text-lg font-bold ">Business Model:</div>{" "}
+                  <p>{props.project.busi_model}</p>
+                  <div className="text-lg font-bold">Highlights:</div>{" "}
+                  <p>{props.project.highlights}</p>
+                </div>
+                <div>
+                  <div>
+                    <Image
+                      className="pb-10"
+                      height={1500}
+                      width={1500}
+                      src={props.project.image}
+                      alt="GFG logo imported from public directory"
+                    />
+                    <br />
+                    {user?.isLoggedIn === true && (
+                      <CheckoutForm
+                        id={props.proj_id_num}
+                        userEmail={user.email}
+                      />
+                    )}
+                  </div>
+                </div>
               </div>
-              <div >
-            <div>
-            <Image className="pb-10" height={1500} width={1500} src={props.project.image} alt="GFG logo imported from public directory"  />
-              <br/>  
-              {user?.isLoggedIn  === true && <CheckoutForm id={props.proj_id_num} userEmail={user.email}/>}
-             </div>
-            </div>
-
-            </div>
-              
-              
-
-        
-              
             </>
           }
-          
         </div>
       </div>
-      
     </div>
   );
 };
