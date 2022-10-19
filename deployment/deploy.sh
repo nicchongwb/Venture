6fb383f95c2b8d82536b4a1528accae39e99179a
nicchongwb <nicholaschongwb@gmail.com>
2022-10-19 22:07:27 +0800

    Fixed Sonar dockercompose

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,9 @@
+# need this for sonarqube - https://hub.docker.com/_/sonarqube
+sysctl -w vm.max_map_count=524288
+sysctl -w fs.file-max=131072
+ulimit -n 131072
+ulimit -u 8192
+
 ## Compose up - Comment out if needed
 cd venture
 docker compose --env-file dockerenv config
