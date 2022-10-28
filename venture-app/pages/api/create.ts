427eb654c58bba1ff07f30dc6f7d63dba7d14cb0
Lyc4on <2000530@sit.singaporetech.edu.sg>
2022-10-29 03:28:01 +0800

    implemented logging for project creation

--- venture-app/pages/api/create.ts
+++ venture-app/pages/api/create.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
-
+import logger from "../../Logger";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const{title,description, highlights,
@@ -22,8 +22,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 email, 
             }
         })
+        logger.info('Project created successfully by :' + req.body.email) 
         res.status(200).json({message: "Project Created"})
     } catch(error){
+        logger.error('Project creation failed by :' + req.body.email) 
         console.log("Failure: "+ error);
     }
 }
