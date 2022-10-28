ac3781396dd46d44b76447b7fc089d5fd871b6e3
Lyc4on <2000530@sit.singaporetech.edu.sg>
2022-10-29 03:30:46 +0800

    implemented logging for invalid csrf

--- venture-app/pages/api/csrf_invalid.ts
+++ venture-app/pages/api/csrf_invalid.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import logger from "../../Logger";
 
 type Data = {
   status: string
@@ -8,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  logger.warn('invalid csrf token submitted')
   res.status(403).send({ status: 'invalid csrf token' });
 }
