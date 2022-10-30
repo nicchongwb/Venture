0fb80427feb30ef7452ef173b880d9e11889006b
nicchongwb <nicholaschongwb@gmail.com>
2022-10-30 16:09:56 +0800

    Updated validation & sanitization for Raise - serverside

--- venture-app/pages/api/create.ts
+++ venture-app/pages/api/create.ts
@@ -1,9 +1,12 @@
 import { prisma } from '../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import { validate } from '../../middleware/createValidate';
+import { createSchema } from '../../schemas/createSchema';
 
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const{title,description, highlights,
         busi_model, image, closingDate,createdAt, updatedAt, email} = req.body
         console.log("req" + req.body)
@@ -28,3 +31,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 }
 
+export default validate(createSchema, handler);
