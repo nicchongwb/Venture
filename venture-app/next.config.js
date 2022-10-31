54dd25f1968a5173d5105c23153e3336e18279af
nicchongwb <nicholaschongwb@gmail.com>
2022-10-31 11:00:10 +0800

    Configured HTTP headers

--- venture-app/next.config.js
+++ venture-app/next.config.js
@@ -41,6 +41,10 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin'
           },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          },
         ],
       },
     ]
