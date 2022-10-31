e3f7925fed0523d371f69a40bc719756a8bdfaa4
nicchongwb <nicholaschongwb@gmail.com>
2022-10-31 10:16:20 +0800

    Configured HTTP headers

--- venture-app/next.config.js
+++ venture-app/next.config.js
@@ -11,19 +11,36 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       headers: [
-  //         {
-  //           key: 'Content-Security-Policy',
-  //           value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          // },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+        ],
+      },
+    ]
+  },
 
 }
 module.exports = nextConfig
